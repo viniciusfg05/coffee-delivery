d69ba9698948d4d7fdd82607019de4b47fb4ebaa
unknown <viniciusserlimpa@gmail.com>
2022-09-09 15:33:06 -0300

    Header Part 1

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,11 @@
+import logo from  '../../assets/logoCoffee.svg'
+
+export function Header() {
+  return (
+    <div>
+      <div>
+        <img src={logo} alt="" />
+      </div>
+    </div>
+  )
+}
--- src/Styles/Global.tsx
+++ src/Styles/Global.tsx
@@ -1,7 +1,31 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
-  body {
-    background: ${(props) => props.theme.background}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
+
+  body {
+    background: ${(props) => props.theme.background};
+    /* color: ${(props) => props.theme['base-text']}; */
+  }
+
+  /* :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px;
+
+  } */
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+
+    -webkit-font-smoothing: antialiased;
+  }
+
+
+
 `
