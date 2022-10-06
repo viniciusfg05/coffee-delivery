85093f10d5e96ace46c066b5a72dd4acd16e912a
Vinicius Ferreira <vinicius.fg05@gmail.com>
2022-10-06 18:40:41 -0300

    Erro - Property 'background' does not exist on type 'DefaultTheme' v2

--- src/Styles/Global.tsx
+++ src/Styles/Global.tsx
@@ -9,7 +9,7 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    background: ${(props) => props.theme['background']};
+    background: #FAFAFA;
     font-size: 2.4rem;
     /* color: ${(props) => props.theme['base-text']}; */
   }
